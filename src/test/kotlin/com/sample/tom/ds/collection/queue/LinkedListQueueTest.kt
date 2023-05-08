3c89740bbf9d6ab69a3b05235c3bce3cf8069ccc
Tom Koptel <tom.koptel@gmail.com>
2023-05-08 15:02:30 +0200

    Add LinkedListQueue

---
+++ src/test/kotlin/com/sample/tom/ds/collection/queue/LinkedListQueueTest.kt
@@ -0,0 +1,30 @@
+package com.sample.tom.ds.collection.queue
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class LinkedListQueueTest {
+    @Test
+    fun `should support enqueue method`() {
+        val queue = linkedListQueueQueueOf(listOf("1", "2", "3", "4"))
+        "${queue.also { it.enqueue("5") }}" shouldBe "1 > 2 > 3 > 4 > 5"
+        "${queue.also { it.enqueue("6") }}" shouldBe "1 > 2 > 3 > 4 > 5 > 6"
+    }
+
+    @Test
+    fun `should support dequeue method`() {
+        val queue = linkedListQueueQueueOf(listOf("1", "2", "3", "4"))
+        queue.dequeue() shouldBe "1"
+        queue.dequeue() shouldBe "2"
+        queue.dequeue() shouldBe "3"
+        queue.dequeue() shouldBe "4"
+        queue.dequeue() shouldBe null
+    }
+
+    @Test
+    fun `peek should not modify the queue`() {
+        val queue = linkedListQueueQueueOf(listOf("1", "2", "3", "4"))
+        queue.peek() shouldBe "1"
+        queue.peek() shouldBe "1"
+    }
+}
