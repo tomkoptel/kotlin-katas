6917f8ac43e79a1a15a789a6ae76bdd792381346
Tom Koptel <tom.koptel@gmail.com>
2023-05-11 19:07:22 +0200

    Add additional cases

--- src/test/kotlin/com/sample/tom/mcdowell/IsStringPermutatedTest.kt
+++ src/test/kotlin/com/sample/tom/mcdowell/IsStringPermutatedTest.kt
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test
 class IsStringPermutatedTest {
     @Test
     fun `the string is a permutation of another`() {
+        "listen".isAPermutationOf("silent") shouldBe true
+        "triangle".isAPermutationOf("integral") shouldBe true
+        "cat".isAPermutationOf("act") shouldBe true
+        "debit card".isAPermutationOf("bad credit") shouldBe true
+
         "abc".isAPermutationOf("cba") shouldBe true
         "aaa".isAPermutationOf("aaa") shouldBe true
         "abc".isAPermutationOf("abc") shouldBe true
