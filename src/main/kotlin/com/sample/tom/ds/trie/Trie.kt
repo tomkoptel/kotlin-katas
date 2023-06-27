215ff02fa328378d432d89b3cb73a3d94123260e
Tom Koptel <tom.koptel@gmail.com>
2023-06-27 15:25:00 +0200

    Add collections for prefixes

--- src/main/kotlin/com/sample/tom/ds/trie/Trie.kt
+++ src/main/kotlin/com/sample/tom/ds/trie/Trie.kt
@@ -13,7 +13,11 @@ class Trie<Key : Any> {
         }
 
         fun Trie<Char>.remove(word: String): Trie<Char> {
-            return  apply { remove(word.toList()) }
+            return apply { remove(word.toList()) }
+        }
+
+        fun Trie<Char>.collections(prefix: String): List<String> {
+            return collections(prefix.toList()).map { it.joinToString(separator = "") }
         }
     }
 
@@ -54,5 +58,29 @@ class Trie<Key : Any> {
             parent.children.remove(current.key)
             current = parent
         }
+    }
+
+    fun collections(prefix: List<Key>): List<List<Key>> {
+        var current = root
+        prefix.forEach { element ->
+            val child = current.children[element] ?: return emptyList()
+            current = child
+        }
+        return collections(prefix, current)
+    }
+
+    private fun collections(prefix: List<Key>, node: TrieNode<Key>): List<List<Key>> {
+        val results = mutableListOf<List<Key>>()
+
+        if (node.isTerminating) {
+            results.add(prefix)
+        }
+
+        node.children.forEach { (key, node) ->
+            val subprefixes = collections(prefix + key, node)
+            results.addAll(subprefixes)
+        }
+
+        return results
     }
 }
--- src/test/kotlin/com/sample/tom/ds/trie/TrieTest.kt
+++ src/test/kotlin/com/sample/tom/ds/trie/TrieTest.kt
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test
 import com.sample.tom.ds.trie.Trie.Companion.insert
 import com.sample.tom.ds.trie.Trie.Companion.contains
 import com.sample.tom.ds.trie.Trie.Companion.remove
+import com.sample.tom.ds.trie.Trie.Companion.collections
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
 
 class TrieTest {
     @Test
@@ -25,5 +27,25 @@ class TrieTest {
         trie.insert("cart")
         trie.remove("cart")
         trie.contains("cart").shouldBeFalse()
+    }
+
+    @Test
+    fun `supports listing of the prefixes`() {
+        val trie = Trie<Char>().apply {
+            insert("car")
+            insert("card")
+            insert("care")
+            insert("cared")
+            insert("cars")
+            insert("carbs")
+            insert("carapace")
+            insert("cargo")
+        }
+
+        val prefixedWithCar = trie.collections("car")
+        prefixedWithCar shouldBe listOf("car", "carapace", "carbs", "cars", "card", "care", "cared", "cargo")
+
+        val prefixedWithCare = trie.collections("care")
+        prefixedWithCare shouldBe listOf("care", "cared")
     }
 }
