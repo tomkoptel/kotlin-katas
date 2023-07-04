d3d948cb51e24cc677e65ef9fdc797026e97de41
Tom Koptel <tom.koptel@gmail.com>
2023-07-04 16:55:08 +0200

    Add notes

--- src/main/kotlin/com/sample/tom/ds/heap/Heap.kt
+++ src/main/kotlin/com/sample/tom/ds/heap/Heap.kt
@@ -1,5 +1,14 @@
 package com.sample.tom.ds.heap
 
+/**
+ * Is a perfect binary tree with all levels field except the last one.
+ * The primary invariant:
+ * * the parent is greater or equal for max heap
+ * * the parent is less or equal for min heap
+ */
 interface Heap<T: Any> : Collection<T> {
+    /**
+     * The peek operation is a generalization of methods returning the min or the max depending on the implementation. ”
+     */
     fun peek(): T?
 }
