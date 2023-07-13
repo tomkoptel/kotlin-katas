603a22a63025c600b13c2bfb22c8e4c6d69746c1
Tom Koptel <tom.koptel@gmail.com>
2023-07-13 19:35:59 +0200

    Add selectionSort

--- src/main/kotlin/com/sample/tom/sort/OquadraticSortAlgorithms.kt
+++ src/main/kotlin/com/sample/tom/sort/OquadraticSortAlgorithms.kt
@@ -25,4 +25,25 @@ object OquadraticSortAlgorithms {
             if (!swapped) return
         }
     }
+
+    fun <T : Comparable<T>> ArrayList<T>.selectionSort() {
+        if (size < 2) return
+
+        for (current in 0 until lastIndex) {
+            var lowest = current
+
+            for (other in (current + 1) until size) {
+                println("current=$current other=$other $this")
+                if (this[other] < this[lowest]) {
+                    lowest = other
+                }
+            }
+
+            if (lowest != current) {
+                Collections.swap(this, lowest, current)
+            }
+
+            println(this)
+        }
+    }
 }
