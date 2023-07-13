90eef71dc749e215b85991aaa208daf81576b29b
tomkoptel <tom.koptel@gmail.com>
2023-07-13 17:48:38 +0200

    Add priority queue based on the heap (#25)

---
+++ src/main/kotlin/com/sample/tom/ds/queue/ComparatorPriorityQueueImpl.kt
@@ -0,0 +1,9 @@
+package com.sample.tom.ds.queue
+
+import com.sample.tom.ds.heap.heapify
+
+class ComparatorPriorityQueueImpl<T : Any>(
+    private val comparator: Comparator<T>,
+) : AbstractPriorityQueue<T>() {
+    override val heap = emptyList<T>().heapify(comparator)
+}
