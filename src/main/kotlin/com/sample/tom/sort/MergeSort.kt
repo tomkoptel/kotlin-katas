181e40977ad4d7f33959de832a0f9698758f8e08
Tom Koptel <tom.koptel@gmail.com>
2023-07-18 21:37:44 +0200

    Setup MergeSort

---
+++ src/main/kotlin/com/sample/tom/sort/MergeSort.kt
@@ -0,0 +1,7 @@
+package com.sample.tom.sort
+
+object MergeSort {
+    fun <T : Comparable<T>> List<T>.mergeSort(): List<T> {
+        return emptyList()
+    }
+}
---
+++ src/test/kotlin/com/sample/tom/sort/MergeSortTest.kt
@@ -0,0 +1,53 @@
+package com.sample.tom.sort
+
+import com.sample.tom.sort.MergeSort.mergeSort
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class MergeSortTest {
+    @Test
+    fun testEmptyList() {
+        val emptyList: List<Int> = emptyList()
+        val sortedList = emptyList.mergeSort()
+        sortedList shouldBe emptyList
+    }
+
+    @Test
+    fun testSortedAscendingList() {
+        val sortedList = listOf(1, 2, 3, 4, 5)
+        val result = sortedList.mergeSort()
+        result shouldBe sortedList
+    }
+
+    @Test
+    fun testSortedDescendingList() {
+        val sortedList = listOf(5, 4, 3, 2, 1)
+        val expected = sortedList.sorted()
+        val result = sortedList.mergeSort()
+        result shouldBe expected
+    }
+
+    @Test
+    fun testUnsortedList() {
+        val unsortedList = listOf(38, 27, 43, 3, 9, 82, 10)
+        val expected = unsortedList.sorted()
+        val result = unsortedList.mergeSort()
+        result shouldBe expected
+    }
+
+    @Test
+    fun testListWithDuplicates() {
+        val listWithDuplicates = listOf(5, 2, 7, 2, 8, 5, 1, 9, 7)
+        val expected = listWithDuplicates.sorted()
+        val result = listWithDuplicates.mergeSort()
+        result shouldBe expected
+    }
+
+    @Test
+    fun testListWithStrings() {
+        val listOfStrings = listOf("apple", "orange", "banana", "grape", "kiwi")
+        val expected = listOfStrings.sorted()
+        val result = listOfStrings.mergeSort()
+        result shouldBe expected
+    }
+}
