d4329af7e9deed0e4ab5aeeb43ffe0ec01031f69
Tom Koptel <tom.koptel@gmail.com>
2023-07-18 22:57:44 +0200

    Rewrite using queue

--- src/main/kotlin/com/sample/tom/sort/MergeSort.kt
+++ src/main/kotlin/com/sample/tom/sort/MergeSort.kt
@@ -4,61 +4,41 @@ import java.util.*
 
 object MergeSort {
     fun <T : Comparable<T>> List<T>.mergeSort(): List<T> {
-        val stack = Stack<List<T>>()
-        stack.pushHalves(this)
+        if (this.size <= 1) return this
 
-        var merged = listOf<T>()
-        while (stack.isNotEmpty()) {
-            val list = stack.pop()
-            if (list.size > 1) {
-                stack.pushHalves(list) // first split
-            } else {
-                // then merge pieces
-                merged = merge(left = merged, right = list)
-            }
+        val queue = LinkedList<List<T>>()
+        forEach { queue.add(listOf(it)) }
+
+        while (queue.size > 1) {
+            val left = queue.remove()
+            val right = queue.remove()
+            val result = merge(left, right)
+            queue.add(result)
         }
 
-        return merged
+        return queue.remove()
     }
 
     private fun <T : Comparable<T>> merge(left: List<T>, right: List<T>): List<T> {
+        val result = mutableListOf<T>()
         var leftIndex = 0
         var rightIndex = 0
-        val result = mutableListOf<T>()
 
         while (leftIndex < left.size && rightIndex < right.size) {
             val leftElement = left[leftIndex]
             val rightElement = right[rightIndex]
 
-            if (leftElement < rightElement) {
-                result += leftElement
-                leftIndex += 1
-            } else if (rightElement < leftElement) {
-                result += rightElement
-                rightIndex += 1
+            if (leftElement <= rightElement) {
+                result.add(leftElement)
+                leftIndex++
             } else {
-                result += leftElement
-                leftIndex += 1
-                result += rightElement
-                rightIndex += 1
+                result.add(rightElement)
+                rightIndex++
             }
         }
 
-        if (leftIndex < left.size) {
-            result.addAll(left.subList(leftIndex, left.size))
-        }
-        if (rightIndex < right.size) {
-            result.addAll(right.subList(rightIndex, right.size))
-        }
-
+        result.addAll(left.subList(leftIndex, left.size))
+        result.addAll(right.subList(rightIndex, right.size))
         return result
-    }
-
-    private fun <T : Comparable<T>> Stack<List<T>>.pushHalves(list: List<T>): Stack<List<T>> {
-        val left = list.subList(0, list.size / 2)
-        val right = list.subList(list.size / 2, list.size)
-        add(left)
-        add(right)
-        return this
     }
 }
