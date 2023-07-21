bace64667950e448a6f0e1184bf921e0da0b55e8
Tom Koptel <tom.koptel@gmail.com>
2023-07-21 23:25:56 +0200

    Add lexicographicalSort

--- src/main/kotlin/com/sample/tom/sort/RadixSort.kt
+++ src/main/kotlin/com/sample/tom/sort/RadixSort.kt
@@ -1,5 +1,8 @@
 package com.sample.tom.sort
 
+import java.lang.Math.pow
+import java.text.FieldPosition
+
 object RadixSort {
     fun MutableList<Int>.radixSort(): MutableList<Int> {
         if (isEmpty()) return this
@@ -27,5 +30,57 @@ object RadixSort {
         }
 
         return this
+    }
+
+    fun MutableList<Int>.lexicographicalSort(): MutableList<Int> {
+        return msdRadixSorted(this, 0)
+    }
+
+    private fun msdRadixSorted(list: MutableList<Int>, position: Int): MutableList<Int> {
+        if (position >= list.maxDigits()) return list
+        val buckets = MutableList<MutableList<Int>>(10) { mutableListOf() }
+        val priorityBucket = arrayListOf<Int>()
+
+        list.forEach { number ->
+            val digit = number.digit(position)
+            if (digit == null) {
+                priorityBucket.add(number)
+            } else {
+                buckets[digit].add(number)
+            }
+        }
+
+        val newValues = buckets.reduce { result, bucket ->
+            if (bucket.isEmpty()) return@reduce result
+            result.addAll(msdRadixSorted(bucket, position + 1))
+            result
+        }
+        priorityBucket.addAll(newValues)
+        return priorityBucket
+    }
+
+    private fun Int.digits(): Int {
+        var count = 0
+        var num = this
+        while (num != 0) {
+            count += 1
+            num /= 10
+        }
+        return count
+    }
+
+    private fun Int.digit(atPosition: Int): Int? {
+        val correctPosition = (atPosition + 1).toDouble()
+        if (correctPosition > digits()) return null
+
+        var num = this
+        while (num / (pow(10.0, correctPosition).toInt()) != 0) {
+            num /= 10
+        }
+        return num % 10
+    }
+
+    private fun List<Int>.maxDigits(): Int {
+        return maxOrNull()?.digits() ?: 0
     }
 }
--- src/test/kotlin/com/sample/tom/sort/RadixSortTest.kt
+++ src/test/kotlin/com/sample/tom/sort/RadixSortTest.kt
@@ -1,5 +1,6 @@
 package com.sample.tom.sort
 
+import com.sample.tom.sort.RadixSort.lexicographicalSort
 import com.sample.tom.sort.RadixSort.radixSort
 import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.shouldBe
@@ -29,5 +30,11 @@ class RadixSortTest {
     @Test
     fun testListWithDuplicates() {
         mutableListOf(5, 2, 7, 2, 8, 5, 1, 9, 7).radixSort() shouldBe mutableListOf(1, 2, 2, 5, 5, 7, 7, 8, 9)
+    }
+
+    @Test
+    fun lexicographicalSort() {
+        val list = mutableListOf(448, 3168, 6217, 7117, 1256, 3887, 3900, 3444, 4976, 6891, 4682)
+        list.lexicographicalSort() shouldBe mutableListOf(1256, 3168, 3444, 3887, 3900, 448, 4682, 4976, 6217, 6891, 7117)
     }
 }
