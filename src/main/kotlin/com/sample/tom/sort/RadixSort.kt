5de5af4009e087afb6d619b098760d08478d4284
tomkoptel <tom.koptel@gmail.com>
2023-08-02 15:05:33 +0200

    Add radix sort MSD + LSD(lexicographical) (#28)

---
+++ src/main/kotlin/com/sample/tom/sort/RadixSort.kt
@@ -0,0 +1,125 @@
+package com.sample.tom.sort
+
+import java.lang.Math.pow
+
+object RadixSort {
+    fun MutableList<Int>.radixSort(): MutableList<Int> {
+        if (isEmpty()) return this
+
+        val base = 10
+        var digits = 1
+        var done = false
+
+        while (!done) {
+            done = true
+
+            val buckets = MutableList<MutableList<Int>>(10) { mutableListOf() }
+            forEach { number ->
+                val remainingPart = number / digits
+                val digit = remainingPart % base
+                buckets[digit] += number
+                if (remainingPart > 0) {
+                    done = false
+                }
+            }
+            digits *= base
+
+            clear()
+            addAll(buckets.flatten())
+        }
+
+        return this
+    }
+
+    fun MutableList<Int>.lexicographicalSort(): MutableList<Int> {
+        val stack = mutableListOf(this to 0)
+        val result = mutableListOf<Int>()
+
+        while (stack.isNotEmpty()) {
+            val (currentList, position) = stack.removeAt(stack.size - 1)
+
+            if (position >= currentList.maxDigits()) {
+                result.addAll(currentList)
+            } else {
+                val buckets = MutableList(10) { mutableListOf<Int>() }
+                val priorityBucket = mutableListOf<Int>()
+
+                currentList.forEach { number ->
+                    val digit = number.digit(atPosition = position)
+
+                    if (digit == null) {
+                        priorityBucket += number
+                    } else {
+                        buckets[digit] += number
+                    }
+                }
+
+                // Add the buckets to the stack in reverse order
+                for (i in 9 downTo 0) {
+                    if (buckets[i].isNotEmpty()) {
+                        stack.add(buckets[i] to position + 1)
+                    }
+                }
+
+                // Add the priority bucket to the stack for further processing
+                if (priorityBucket.isNotEmpty()) {
+                    stack.add(priorityBucket to position + 1)
+                }
+            }
+        }
+
+        return result
+    }
+
+    fun MutableList<Int>.lexicographicalSortRecursive(): MutableList<Int> {
+        return msdRadixSorted(this, 0)
+    }
+
+    private fun msdRadixSorted(list: MutableList<Int>, position: Int): MutableList<Int> {
+        if (position >= list.maxDigits()) return list
+        val buckets = MutableList<MutableList<Int>>(10) { mutableListOf() }
+        val priorityBucket = arrayListOf<Int>()
+
+        list.forEach { number ->
+            val digit = number.digit(position)
+            if (digit == null) {
+                priorityBucket.add(number)
+            } else {
+                buckets[digit].add(number)
+            }
+        }
+
+        val newValues = buckets.reduce { result, bucket ->
+            if (bucket.isEmpty()) return@reduce result
+            result.addAll(msdRadixSorted(bucket, position + 1))
+            result
+        }
+        priorityBucket.addAll(newValues)
+        return priorityBucket
+    }
+
+    private fun Int.digits(): Int {
+        var count = 0
+        var num = this
+        while (num != 0) {
+            count += 1
+            num /= 10
+        }
+        return count
+    }
+
+    private fun Int.digit(atPosition: Int): Int? {
+        val correctPosition = (atPosition + 1).toDouble()
+        if (correctPosition > digits()) return null
+
+        var num = this
+        while (num / (pow(10.0, correctPosition).toInt()) != 0) {
+            num /= 10
+        }
+        return num % 10
+    }
+
+    private fun List<Int>.maxDigits(): Int {
+        return maxOrNull()?.digits() ?: 0
+    }
+}
---
+++ src/test/kotlin/com/sample/tom/sort/RadixSortTest.kt
@@ -0,0 +1,59 @@
+package com.sample.tom.sort
+
+import com.sample.tom.sort.RadixSort.lexicographicalSort
+import com.sample.tom.sort.RadixSort.lexicographicalSortRecursive
+import com.sample.tom.sort.RadixSort.radixSort
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class RadixSortTest {
+    @Test
+    fun testEmptyList() {
+        mutableListOf<Int>().radixSort().shouldBeEmpty()
+    }
+
+    @Test
+    fun testSortedAscendingList() {
+        mutableListOf(1, 2, 3, 4, 5).radixSort() shouldBe mutableListOf(1, 2, 3, 4, 5)
+    }
+
+    @Test
+    fun testSortedDescendingList() {
+        mutableListOf(5, 4, 3, 2, 1).radixSort() shouldBe mutableListOf(1, 2, 3, 4, 5)
+    }
+
+    @Test
+    fun testUnsortedList() {
+        mutableListOf(38, 27, 43, 3, 9, 82, 10).radixSort() shouldBe mutableListOf(3, 9, 10, 27, 38, 43, 82)
+    }
+
+    @Test
+    fun testListWithDuplicates() {
+        mutableListOf(5, 2, 7, 2, 8, 5, 1, 9, 7).radixSort() shouldBe mutableListOf(1, 2, 2, 5, 5, 7, 7, 8, 9)
+    }
+
+    @Test
+    fun lexicographicalSort() {
+        val list = mutableListOf(448, 3168, 6217, 7117, 1256, 3887, 3900, 3444, 4976, 6891, 4682)
+        list.lexicographicalSort() shouldBe mutableListOf(1256, 3168, 3444, 3887, 3900, 448, 4682, 4976, 6217, 6891, 7117)
+    }
+
+    @Test
+    fun lexicographicalSortSmall() {
+        val list = mutableListOf(500, 1345, 13, 459, 44)
+        list.lexicographicalSort() shouldBe mutableListOf(13, 1345, 44, 459, 500)
+    }
+
+    @Test
+    fun lexicographicalSortRecursive() {
+        val list = mutableListOf(448, 3168, 6217, 7117, 1256, 3887, 3900, 3444, 4976, 6891, 4682)
+        list.lexicographicalSortRecursive() shouldBe mutableListOf(1256, 3168, 3444, 3887, 3900, 448, 4682, 4976, 6217, 6891, 7117)
+    }
+
+    @Test
+    fun lexicographicalSortRecursiveSmall() {
+        val list = mutableListOf(500, 1345, 13, 459, 44)
+        list.lexicographicalSortRecursive() shouldBe mutableListOf(13, 1345, 44, 459, 500)
+    }
+}
