d77affef8a46a4373a2d0b488a3994dc8406adf4
tomkoptel <tom.koptel@gmail.com>
2023-08-27 13:31:54 +0200

    Add quick sort (#31)

---
+++ src/main/kotlin/com/sample/tom/sort/QuickSort.kt
@@ -0,0 +1,128 @@
+package com.sample.tom.sort
+
+import java.util.*
+
+object QuickSort {
+    fun <T : Comparable<T>> List<T>.naiveQuickSort(): List<T> {
+        val stack = LinkedList<List<T>>().also { it.add(this) }
+        val result = LinkedList<T>()
+        while (stack.isNotEmpty()) {
+            val list = stack.pop()
+            if (list.size > 1) {
+                val pivot = list[list.size / 2]
+                val less = list.filter { it < pivot }
+                val more = list.filter { it > pivot }
+                stack.push(more)
+                stack.push(listOf(pivot))
+                stack.push(less)
+            } else {
+                list.firstOrNull()?.let { result.add(it) }
+            }
+            println(stack)
+        }
+        return result
+    }
+
+    fun <T : Comparable<T>> List<T>.lomutoQuickSort(): List<T> {
+        if (size < 2) return this
+        val stack = LinkedList<Pair<Int, Int>>()
+        stack.push(0 to size - 1)
+        while (stack.isNotEmpty()) {
+            val (low, high) = stack.pop()
+            if (low < high) {
+                val pivotIndex = medianOfThree(low, high)
+                Collections.swap(this, pivotIndex, high)
+
+                val pivot = this[high]
+                var i = low
+                for (j in low until high) {
+                    if (this[j] <= pivot) {
+                        Collections.swap(this, i, j)
+                        i += 1
+                    }
+                }
+                Collections.swap(this, i, high)
+                stack.push(i + 1 to high)
+                stack.push(low to i - 1)
+            }
+        }
+        return this
+    }
+
+    fun <T : Comparable<T>> List<T>.hoareQuickSort(): List<T> {
+        if (size < 2) return this
+        val stack = LinkedList<Pair<Int, Int>>()
+        stack.push(0 to size - 1)
+        while (stack.isNotEmpty()) {
+            val (low, high) = stack.pop()
+            if (low < high) {
+                val pivotIndex = medianOfThree(low, high)
+                Collections.swap(this, pivotIndex, high)
+
+                val pivot = this[low]
+                var i = low - 1
+                var j = high + 1
+                while (true) {
+                    do {
+                        j -= 1
+                    } while (this[j] > pivot)
+                    do {
+                        i += 1
+                    } while (this[i] < pivot)
+                    if (i < j) {
+                        Collections.swap(this, i, j)
+                    } else {
+                        break
+                    }
+                }
+                stack.push(j + 1 to high)
+                stack.push(low to j)
+            }
+        }
+        return this
+    }
+
+    fun <T : Comparable<T>> List<T>.dutchFlagQuickSort(): List<T> {
+        if (size < 2) return this
+        val stack = LinkedList<Pair<Int, Int>>()
+        stack.push(0 to size - 1)
+        while (stack.isNotEmpty()) {
+            val (low, high) = stack.pop()
+            if (low < high) {
+                var smaller = low
+                var larger = high
+                var equal = low
+                val pivot = this[high]
+                while (equal <= larger) {
+                    if (this[equal] < pivot) {
+                        Collections.swap(this, smaller, equal)
+                        smaller++
+                        equal++
+                    } else if (this[equal] == pivot) {
+                        equal++
+                    } else {
+                        Collections.swap(this, equal, larger)
+                        larger--
+                    }
+                }
+                stack.push(low + 1 to high)
+                stack.push(low to larger - 1)
+            }
+        }
+        return this
+    }
+
+    private fun <T : Comparable<T>> List<T>.medianOfThree(low: Int, high: Int): Int {
+        val center = (low + high) / 2
+        if (this[low] > this[center]) {
+            Collections.swap(this, low, center)
+        }
+        if (this[low] > this[high]) {
+            Collections.swap(this, low, high)
+        }
+        if (this[center] > this[high]) {
+            Collections.swap(this, center, high)
+        }
+        return center
+    }
+}
---
+++ src/test/kotlin/com/sample/tom/sort/QuickSortTest.kt
@@ -0,0 +1,40 @@
+package com.sample.tom.sort
+
+import com.sample.tom.sort.QuickSort.dutchFlagQuickSort
+import com.sample.tom.sort.QuickSort.hoareQuickSort
+import com.sample.tom.sort.QuickSort.lomutoQuickSort
+import com.sample.tom.sort.QuickSort.naiveQuickSort
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class QuickSortTest {
+    @Test
+    fun naiveQuickSor() {
+        val unsorted = listOf(10, 9, 8, 7, 6, 3)
+        unsorted.naiveQuickSort() shouldBe listOf(3, 6, 7, 8, 9, 10)
+    }
+
+    @Test
+    fun lomutoQuickSort1() {
+        val unsorted = listOf(0, 7, 8, 4, 3)
+        unsorted.lomutoQuickSort() shouldBe listOf(0, 3, 4, 7, 8)
+    }
+
+    @Test
+    fun lomutoQuickSort2() {
+        val unsorted = listOf(12, 0, 3, 9, 2, 21, 18, 27, 1, 5, 8, -1, 8)
+        unsorted.lomutoQuickSort() shouldBe listOf(-1, 0, 1, 2, 3, 5, 8, 8, 9, 12, 18, 21, 27)
+    }
+
+    @Test
+    fun hoareQuickSort() {
+        val unsorted = listOf(12, 0, 3, 9, 2, 21, 18, 27, 1, 5, 8, -1, 8)
+        unsorted.hoareQuickSort() shouldBe listOf(-1, 0, 1, 2, 3, 5, 8, 8, 9, 12, 18, 21, 27)
+    }
+
+    @Test
+    fun dutchFlagQuickSort() {
+        val unsorted = listOf(12, 0, 3, 9, 2, 21, 18, 27, 1, 5, 8, -1, 8)
+        unsorted.dutchFlagQuickSort() shouldBe listOf(-1, 0, 1, 2, 3, 5, 8, 8, 9, 12, 18, 21, 27)
+    }
+}
