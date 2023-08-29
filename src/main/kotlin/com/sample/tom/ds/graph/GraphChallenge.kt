3f5356f5c787a9eb1cea32cee39792f6dc2dc482
Tom Koptel <tom.koptel@gmail.com>
2023-08-29 22:11:01 +0200

    Fix cyclic dependency

--- src/main/kotlin/com/sample/tom/ds/graph/GraphChallenge.kt
+++ src/main/kotlin/com/sample/tom/ds/graph/GraphChallenge.kt
@@ -4,29 +4,35 @@ import java.util.LinkedList
 import java.util.concurrent.atomic.AtomicInteger
 
 object GraphChallenge {
-    fun <T: Any> Graph<T>.distanceNonRecursive(vertexA: Vertex<T>, vertexB: Vertex<T>): Int {
+    fun <T : Any> Graph<T>.distanceNonRecursive(vertexA: Vertex<T>, vertexB: Vertex<T>): Int {
         val stack = LinkedList<Edge<T>>()
         edges(vertexA).forEach { stack.push(it) }
+        val visited = mutableSetOf<Vertex<T>>()
         var paths = 0
         while (stack.isNotEmpty()) {
             val edge = stack.pop()
-            if (edge.destination == vertexB) {
+            val source = edge.source
+            val destination = edge.destination
+            if (destination == vertexB) {
                 paths++
             } else {
-                edges(edge.destination).forEach { stack.push(it) }
+                if (destination !in visited) {
+                    edges(destination).forEach { stack.push(it) }
+                    visited.add(source)
+                }
             }
         }
         return paths
     }
 
-    fun <T: Any> Graph<T>.distanceRecursive(vertexA: Vertex<T>, vertexB: Vertex<T>): Int {
+    fun <T : Any> Graph<T>.distanceRecursive(vertexA: Vertex<T>, vertexB: Vertex<T>): Int {
         val numberOfPaths = AtomicInteger(0)
         val visited = mutableSetOf<Vertex<T>>()
         paths(vertexA, vertexB, visited, numberOfPaths)
         return numberOfPaths.get()
     }
 
-    private fun <T: Any> Graph<T>.paths(
+    private fun <T : Any> Graph<T>.paths(
         source: Vertex<T>,
         destination: Vertex<T>,
         visited: MutableSet<Vertex<T>>,
--- src/test/kotlin/com/sample/tom/ds/graph/GraphChallengeTest.kt
+++ src/test/kotlin/com/sample/tom/ds/graph/GraphChallengeTest.kt
@@ -6,25 +6,38 @@ import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class GraphChallengeTest {
+    private val graph = AdjacencyList<String>()
+    private val vertexA = graph.createVertex("A")
+    private val vertexB = graph.createVertex("B")
+    private val vertexC = graph.createVertex("C")
+    private val vertexD = graph.createVertex("D")
+    private val vertexE = graph.createVertex("E")
+
     @Test
-    fun `distance between vertices non recursive`() {
-        val (graph, vertexA, vertexE) = pepareTestData()
+    fun `for non cyclic distance between vertices non recursive`() {
+        populateNonCyclicGraph()
         graph.distanceNonRecursive(vertexA, vertexE) shouldBe 5
     }
 
     @Test
-    fun `distance between vertices recursive`() {
-        val (graph, vertexA, vertexE) = pepareTestData()
+    fun `for non cyclic distance between vertices recursive`() {
+        populateNonCyclicGraph()
         graph.distanceRecursive(vertexA, vertexE) shouldBe 5
     }
 
-    private fun pepareTestData(): Triple<AdjacencyList<String>, Vertex<String>, Vertex<String>> {
-        val graph = AdjacencyList<String>()
-        val vertexA = graph.createVertex("A")
-        val vertexB = graph.createVertex("B")
-        val vertexC = graph.createVertex("C")
-        val vertexD = graph.createVertex("D")
-        val vertexE = graph.createVertex("E")
+    @Test
+    fun `for cyclic distance between vertices non recursive`() {
+        populateCyclicGraph()
+        graph.distanceNonRecursive(vertexA, vertexE) shouldBe 3
+    }
+
+    @Test
+    fun `for cyclic distance between vertices recursive`() {
+        populateCyclicGraph()
+        graph.distanceRecursive(vertexA, vertexE) shouldBe 3
+    }
+
+    private fun populateNonCyclicGraph() {
         graph.addDirectedEdge(vertexA, vertexB, 0.0)
         graph.addDirectedEdge(vertexA, vertexC, 0.0)
         graph.addDirectedEdge(vertexA, vertexD, 0.0)
@@ -33,6 +46,14 @@ class GraphChallengeTest {
         graph.addDirectedEdge(vertexB, vertexC, 0.0)
         graph.addDirectedEdge(vertexC, vertexE, 0.0)
         graph.addDirectedEdge(vertexD, vertexE, 0.0)
-        return Triple(graph, vertexA, vertexE)
+    }
+
+    private fun populateCyclicGraph() {
+        graph.addUndirectedEdge(vertexA, vertexB, 0.0)
+        graph.addUndirectedEdge(vertexB, vertexC, 0.0)
+        graph.addUndirectedEdge(vertexC, vertexD, 0.0)
+        graph.addUndirectedEdge(vertexD, vertexE, 0.0)
+        graph.addUndirectedEdge(vertexE, vertexA, 0.0)
+        graph.addUndirectedEdge(vertexA, vertexE, 0.0)
     }
 }
