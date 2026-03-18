c23c2db4219ceea810840dcc4906745bd6732a58
Tom Koptel <tom.koptel@gmail.com>
2026-03-18 22:49:57 +0100

    My impl of add at LinkedList2

---
+++ src/test/kotlin/com/sample/tom/ds/list/LinkedList2Test.kt
@@ -0,0 +1,121 @@
+package com.sample.tom.ds.list
+
+import org.junit.jupiter.api.Test
+
+class LinkedList2Test {
+
+    @Test
+    fun name() {
+        val list = LinkedList<Int>()
+        list.add(value = 1)
+        list.add(value =2)
+        list.add(value =3)
+        list.add(value =0, index = 0)
+        list.add(value =99, index = 1)
+        println(list)
+    }
+
+    private class LinkedList<T : Any?> {
+        private var head: Node<T>? = null
+        private var tail: Node<T>? = null
+        private var length = 0
+
+        fun add(value: T, index: Int = length) {
+            val newNode = Node(
+                value = value,
+                next = null
+            )
+            if (head == null) {
+                head = newNode
+                tail = newNode
+                length++
+                return
+            }
+
+            if (index == 0) {
+                newNode.next = head
+                head = newNode
+                length++
+                return
+            }
+
+            if (index >= length) {
+                tail?.next = newNode
+                tail = newNode
+            } else {
+                var step = 0
+                var node = head
+                while (step != index - 1) {
+                    node = node?.next
+                    step++
+                }
+                if (node != null) {
+                    val currentNodesNext = node.next
+                    newNode.next = currentNodesNext
+                    node.next = newNode
+                }
+            }
+            length++
+        }
+
+        fun remove(index: Int) {}
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as LinkedList<*>
+
+            return head == other.head
+        }
+
+        override fun hashCode(): Int {
+            return head?.hashCode() ?: 0
+        }
+
+        override fun toString(): String {
+            var previousNode: Node<T>? = null
+            var nextNode = head
+            val string = StringBuilder()
+            while (nextNode != null) {
+                if (previousNode == null) {
+                    string.append(nextNode.value)
+                } else {
+                    string.append(" -> ").append(nextNode.value)
+                }
+                previousNode = nextNode
+                nextNode = nextNode.next
+            }
+
+            return "length: $length [$string] tail=$tail"
+        }
+
+        private class Node<T : Any?>(
+            val value: T? = null,
+            var next: Node<T>? = null,
+        ) {
+
+            override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as Node<*>
+
+                if (value != other.value) return false
+                if (next != other.next) return false
+
+                return true
+            }
+
+            override fun hashCode(): Int {
+                var result = value?.hashCode() ?: 0
+                result = 31 * result + (next?.hashCode() ?: 0)
+                return result
+            }
+
+            override fun toString(): String {
+                return "Node(value=$value, next=$next)"
+            }
+        }
+    }
+}
