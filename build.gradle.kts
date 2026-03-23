8bbd6020e304150b9906e2d30632b35e97e12bc6
tomkoptel <tom.koptel@gmail.com>
2026-03-23 10:41:41 +0100

    Add ChannelTest examples from https://kotlinlang.org/docs/channels.html (#47)

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,10 +37,11 @@ kotlin {
 }
 
 dependencies {
-    implementation("org.testng:testng:7.1.0")
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation(libs.kotest.core)
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 }
 
