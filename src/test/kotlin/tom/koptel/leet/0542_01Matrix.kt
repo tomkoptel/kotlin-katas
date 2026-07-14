080e335888d877a7883ba4c67ad6b966aa6e13f4
Tom Koptel <tom.koptel@gmail.com>
2026-07-14 20:30:21 +0200

    less verbosity

--- src/test/kotlin/tom/koptel/leet/0542_01Matrix.kt
+++ src/test/kotlin/tom/koptel/leet/0542_01Matrix.kt
@@ -22,75 +22,35 @@ class `0542_01Matrix` {
 
     private class Solution {
         fun updateMatrix(mat: Array<IntArray>): Array<IntArray> {
-            val seed = ArrayDeque<Cell>()
-            val distance: Array<IntArray> = Array(mat.size) {
-                IntArray(mat[0].size) { -1 }
-            }
-            for (row in (0 until mat.size)) {
-                for (cell in (0 until mat[row].size)) {
-                    if (mat[row][cell] == 0) {
-                        distance[row][cell] = 0
-                        seed.add(Cell(row = row, cell = cell, mat = mat))
-                    }
+            val seed = ArrayDeque<Pair<Int, Int>>()
+            val rows = mat.size
+            val cols = mat[0].size
+            val directions = listOf((-1 to 0), (1 to 0), (0 to -1), (0 to 1))
+            val distance: Array<IntArray> = Array(rows) { r ->
+                IntArray(cols) { c ->
+                    val el = mat[r][c]
+                    if (el == 0) seed.add((r to c))
+                    if (el == 0) 0 else -1
                 }
             }
 
             while (seed.isNotEmpty()) {
-                val seedCell = seed.removeFirst()
-                seedCell.down()?.let { (row, cell) ->
-                    seedCell.populateDistance(row = row, cell = cell, distance = distance)?.let(seed::add)
-                }
-                seedCell.up()?.let { (row, cell) ->
-                    seedCell.populateDistance(row = row, cell = cell, distance = distance)?.let(seed::add)
-                }
-                seedCell.left()?.let { (row, cell) ->
-                    seedCell.populateDistance(row = row, cell = cell, distance = distance)?.let(seed::add)
-                }
-                seedCell.right()?.let { (row, cell) ->
-                    seedCell.populateDistance(row = row, cell = cell, distance = distance)?.let(seed::add)
+                val (row, col) = seed.removeFirst()
+                for ((dr, dc) in directions) {
+                    val nr = row + dr
+                    val nc = col + dc
+                    if (
+                        (nr in 0 until rows) &&
+                        (nc in 0 until cols) &&
+                        distance[nr][nc] == -1
+                    ) {
+                        distance[nr][nc] = distance[row][col] + 1
+                        seed.add((nr to nc))
+                    }
                 }
             }
 
             return distance
-        }
-
-        private class Cell(
-            val row: Int,
-            val cell: Int,
-            private val mat: Array<IntArray>,
-        ) {
-            fun populateDistance(row: Int, cell: Int, distance: Array<IntArray>): Cell? {
-                val el = distance[row][cell]
-                return if (el == -1) {
-                    distance[row][cell] = distance[this.row][this.cell] + 1
-                    Cell(row = row, cell = cell, mat = mat)
-                } else {
-                    null
-                }
-            }
-
-            fun down(): Pair<Int, Int>? {
-                if (row + 1 < rowNum) return (row + 1) to cell
-                return null
-            }
-
-            fun up(): Pair<Int, Int>? {
-                if (row - 1 >= 0) return (row - 1) to cell
-                return null
-            }
-
-            fun left(): Pair<Int, Int>? {
-                if (cell - 1 >= 0) return row to (cell - 1)
-                return null
-            }
-
-            fun right(): Pair<Int, Int>? {
-                if (cell + 1 < cellNum) return row to (cell + 1)
-                return null
-            }
-
-            private val rowNum: Int get() = mat.size
-            private val cellNum: Int get() = mat[0].size
         }
     }
 }
