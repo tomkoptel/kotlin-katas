511c3f51783b9e2639f92749d62410d8bc92d9ae
Tom Koptel <tom.koptel@gmail.com>
2026-07-15 22:59:41 +0200

    add house robber solution

---
+++ src/test/kotlin/tom/koptel/leet/0198_HouseRobber.kt
@@ -0,0 +1,27 @@
+package tom.koptel.leet
+
+import org.junit.jupiter.api.Test
+
+import io.kotest.matchers.shouldBe
+
+class `0198_HouseRobber` {
+    @Test
+    fun name() {
+        val nums = intArrayOf(1, 2, 3, 1)
+        Solution().rob(nums) shouldBe 4
+    }
+
+    private class Solution {
+        fun rob(nums: IntArray): Int {
+            val best = IntArray(nums.size) { 0 }
+            best[0] = nums[0]
+            best[1] = maxOf(nums[0], nums[1])
+            for (i in 2 until nums.size) {
+                val robIt = nums[i] + best[i - 2]
+                val skipIt = best[i - 1]
+                best[i] = maxOf(robIt, skipIt)
+            }
+            return best.last()
+        }
+    }
+}
